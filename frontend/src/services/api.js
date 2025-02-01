326b6fdd590f521180b87ab430c6588b5d0dca47
ritesh <ritesh25033@gmail.com>
2025-02-01 23:13:26 +0530

    backend deployed

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,4 +1,6 @@
-const API_URL = "http://localhost:5000/api"
+// const API_URL = "http://localhost:5000/api"
+
+const API_URL= "https://searchbar-3xns.onrender.com/api"
 
 export const searchStudents = async (query) => {
   const response = await fetch(`${API_URL}/students?query=${query}`)
